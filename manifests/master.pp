7f738921cebfea3f92047cee6ae42b2ceaa8363a
Dan Hardiker <dan@hardiker.co.uk>
2014-05-03 15:14:26 +0100

    Removing the quotes from the variables

--- manifests/master.pp
+++ manifests/master.pp
@@ -4,14 +4,14 @@ class wal_e::master {
   cron { 'wal_e-backup-push':
     command => "/usr/bin/envdir ${wal_e::rootdir}/env /usr/local/bin/wal-e backup-push ${wal_e::pgdata}",
     user    => 'postgres',
-    minute  => '${wal_e::master_cron_backup_minute}',
-    hour    => '${wal_e::master_cron_backup_hour}',
+    minute  => $wal_e::master_cron_backup_minute,
+    hour    => $wal_e::master_cron_backup_hour,
   }
   cron { 'wal_e-delete-retain':
     command => "/usr/bin/envdir ${wal_e::rootdir}/env /usr/local/bin/wal-e delete --confirm retain 5",
     user    => 'postgres',
-    minute  => '${wal_e::master_cron_delete_minute}',
-    hour    => '${wal_e::master_cron_delete_hour}',
+    minute  => $wal_e::master_cron_delete_minute,
+    hour    => $wal_e::master_cron_delete_hour,
 
   file_line { 'postgres_conf_1':
     path => "${wal_e::pgconf}/postgresql.conf",
